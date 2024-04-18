2148e6b24babd198d0dd88a48180c82c9d4d65ae
Laura Delgado <Laura Delgado@DESKTOP-S1FFKE2>
2024-04-18 17:11:34 +0200

    Primer Commit

---
+++ Buenosdiasnoches/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Buenosdiasnoches {
+}
---
+++ Buenosdiasnoches/src/principal/BuenasTardes.java
@@ -0,0 +1,9 @@
+package principal;
+
+public class BuenasTardes {
+
+	public static void main(String[] args) {
+		System.out.println("Buenas Tardes");
+	}
+
+}
